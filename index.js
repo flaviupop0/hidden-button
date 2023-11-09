136133cacbe25dcee63098f69c8e23f55b3c8bf4
flaviupop0 <flaviupop2001@gmail.com>
2023-11-09 17:17:55 +0200

    indentation fix

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-let noButtons, buttonsPress = 0, winnerIndex;
+let noButtons,
+    buttonsPress = 0,
+    winnerIndex;
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("generate").addEventListener("click", function() {
-        noButtons = parseInt(document.getElementById('numberButtons').value);
+    document.getElementById("generate").addEventListener("click", function () {
+        noButtons = parseInt(document.getElementById("numberButtons").value);
         if (noButtons <= 2) {
             alert("Please enter more than 2 buttons to generate");
         } else {
             generateButtons();
         }
-    })
-})  
+    });
+});
 
 function generateButtons() {
-    const buttonContainer = document.getElementById('buttonContainer');
-    document.getElementById('body').style.backgroundColor = 'white';
+    const buttonContainer = document.getElementById("buttonContainer");
+    document.getElementById("body").style.backgroundColor = "white";
     setTimeout(function() {
         winnerIndex = Math.floor(Math.random() * noButtons);
-        buttonContainer.innerHTML = '';
+        buttonContainer.innerHTML = "";
         buttonGenerator(buttonContainer);
     }, 10);
 }
 
 function buttonGenerator(container) {
     for (let i = 0; i < noButtons; ++i) {
-        const button = document.createElement('button');
-        button.textContent = 'Am I the choosen one?';
-        button.classList.add('check');
+        const button = document.createElement("button");
+        button.textContent = "Am I the choosen one?";
+        button.classList.add("check");
         container.appendChild(button);
-        const buttons = buttonContainer.getElementsByTagName('button');
+        const buttons = buttonContainer.getElementsByTagName("button");
         checkWinner(i, buttons, button);
     }
 }
@@ -35,23 +37,25 @@ function buttonGenerator(container) {
 function checkWinner(index, buttons, button) {
     if (index == winnerIndex) {
         button.addEventListener("click", function() {
-            button.textContent = 'You Won! 🎉'
-            document.getElementById('body').style.backgroundColor = 'green';
+            button.textContent = "You Won! 🎉";
+            document.getElementById("body").style.backgroundColor = "green";
             disableButtons(buttons);
             buttonsPress = 0;
-        })
+        });
     } else {
         button.addEventListener("click", function() {
             ++buttonsPress;
-            button.textContent = 'That\'s not the right one 🥺';
-            document.getElementById('body').style.backgroundColor = 'red';
+            button.textContent = "That's not the right one 🥺";
+            document.getElementById("body").style.backgroundColor = "red";
             if (buttonsPress == 3) {
                 buttonsPress = 0;
                 disableButtons(buttons);
-                alert('You lost, try again by generating a new number of buttons');
+                alert(
+                    "You lost, try again by generating a new number of buttons"
+                );
             }
             button.disabled = true;
-        })
+        });
     }
 }
 
@@ -60,4 +64,3 @@ function disableButtons(buttons) {
         buttons[j].disabled = true;
     }
 }
-
