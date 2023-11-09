7a09837a1034d10b9189c44eaf4bcbf1375cec93
flaviupop0 <flaviupop2001@gmail.com>
2023-11-09 16:55:22 +0200

    new functions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-let noButtons;
-let buttonsPress = 0;
+let noButtons, buttonsPress = 0, winnerIndex
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("generate").addEventListener("click", function() {
@@ -16,35 +15,42 @@ function generateButtons() {
     const buttonContainer = document.getElementById('buttonContainer');
     document.getElementById('body').style.backgroundColor = 'white';
     setTimeout(function() {
+        winnerIndex = Math.floor(Math.random() * noButtons);
         buttonContainer.innerHTML = '';
-        const winnerIndex = Math.floor(Math.random() * noButtons);
-        for (let i = 0; i < noButtons; ++i) {
-            const button = document.createElement('button');
-            button.textContent = 'Am I the choosen one?';
-            button.classList.add('check');
-            buttonContainer.appendChild(button);
-            const buttons = buttonContainer.getElementsByTagName('button');
-            if (i == winnerIndex) {
-                button.addEventListener("click", function() {
-                    button.textContent = 'You Won! 🎉'
-                    document.getElementById('body').style.backgroundColor = 'green';
-                    disableButtons(buttons);
-                })
-            } else {
-                button.addEventListener("click", function() {
-                    ++buttonsPress;
-                    console.log(buttonsPress);
-                    button.textContent = 'That\'s not the right one 🥺';
-                    document.getElementById('body').style.backgroundColor = 'red';
-                    if (buttonsPress == 3) {
-                        disableButtons(buttons);
-                        buttonsPress = 0;
-                        alert('You lost, try again by generating a new number of buttons');
-                    }
-                })
-            }
-        }
+        buttonGenerator(buttonContainer);
     }, 10);
+}
+
+function buttonGenerator(container) {
+    for (let i = 0; i < noButtons; ++i) {
+        const button = document.createElement('button');
+        button.textContent = 'Am I the choosen one?';
+        button.classList.add('check');
+        container.appendChild(button);
+        const buttons = buttonContainer.getElementsByTagName('button');
+        checkWinner(i, buttons, button);
+    }
+}
+
+function checkWinner(index, buttons, button) {
+    if (index == winnerIndex) {
+        button.addEventListener("click", function() {
+            button.textContent = 'You Won! 🎉'
+            document.getElementById('body').style.backgroundColor = 'green';
+            disableButtons(buttons);
+        })
+    } else {
+        button.addEventListener("click", function() {
+            ++buttonsPress;
+            button.textContent = 'That\'s not the right one 🥺';
+            document.getElementById('body').style.backgroundColor = 'red';
+            if (buttonsPress == 3) {
+                disableButtons(buttons);
+                buttonsPress = 0;
+                alert('You lost, try again by generating a new number of buttons');
+            }
+        })
+    }
 }
 
 function disableButtons(buttons) {
